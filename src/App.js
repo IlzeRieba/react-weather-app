dafcbd813587dd64175dccdccd15cb9f830b95e8
IlzeRieba <78449581+IlzeRieba@users.noreply.github.com>
2022-06-24 11:41:28 +0200

    removed content to fix issue with the site deploy

--- src/App.js
+++ src/App.js
@@ -1,28 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>React Weather App</h1>
-
-      <footer>
-        This app was coded by Ilze Rieba and is
-        <a
-          href="https://github.com/IlzeRieba/react-weather-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {" "}
-          open-sourced on GitHub{" "}
-        </a>{" "}
-        and hosted on{" "}
-        <a href="" target="_blank" rel="noopener noreferrer">
-          {" "}
-          Netlify{" "}
-        </a>
-        .
-      </footer>
     </div>
   );
 }
