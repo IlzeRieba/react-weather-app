30009149492b8fd02543bd47d2987659af46a0bd
IlzeRieba <78449581+IlzeRieba@users.noreply.github.com>
2022-06-24 11:21:03 +0200

    added repository

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Weather App</h1>
+
+      <footer>
+        This app was coded by Ilze Rieba and is
+        <a href=""> open-sourced on GitHub </a> and hosted on{" "}
+        <a href=""> Netlify </a>.
+      </footer>
     </div>
   );
 }
