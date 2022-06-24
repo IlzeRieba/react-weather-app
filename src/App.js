62fd42603928b75b8ebd33b24dafcd74d045d489
IlzeRieba <78449581+IlzeRieba@users.noreply.github.com>
2022-06-24 14:48:35 +0200

    added html structrure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <h1>React Weather App</h1>
+      <
+        Weather /
+      >
+      <footer>
+        This project was coded by Ilze Rieba and is{" "}
+        <a href="https://github.com/IlzeRieba/react-weather-app">
+          {" "}
+          open-sourced on GitHub{" "}
+        </a>
+      </footer>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./Weather.css";
+
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+ 
+        <form>
+                 <div class="row">
+          <div class="col-9">
+            <input type="search" placeholder="Enter a city..." className="form-control" />
+          </div>
+          <div class="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+           </div>
+        </form>
+         
+     
+      <h1>Seignosse</h1>
+      <ul>
+        <li>Friday 24th June 14:07</li>
+        <li>Sunny</li>
+      </ul>
+
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="sunny icon"
+          />
+          26°C
+        </div>
+
+        <div className="col-6">
+          <ul>
+            <li>Humidity: 20%</li>
+            <li>Wind: 5 km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
