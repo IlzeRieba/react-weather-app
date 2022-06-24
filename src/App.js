be7ec205787e2bdfdbdb246390446c81f067ecf5
IlzeRieba <78449581+IlzeRieba@users.noreply.github.com>
2022-06-24 11:28:13 +0200

    cleaned the code and added heading and footer with links

--- src/App.js
+++ src/App.js
@@ -8,8 +8,20 @@ function App() {
 
       <footer>
         This app was coded by Ilze Rieba and is
-        <a href=""> open-sourced on GitHub </a> and hosted on{" "}
-        <a href=""> Netlify </a>.
+        <a
+          href="https://github.com/IlzeRieba/react-weather-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {" "}
+          open-sourced on GitHub{" "}
+        </a>{" "}
+        and hosted on{" "}
+        <a href="" target="_blank" rel="noopener noreferrer">
+          {" "}
+          Netlify{" "}
+        </a>
+        .
       </footer>
     </div>
   );
