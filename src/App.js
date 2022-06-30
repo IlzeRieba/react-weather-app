6afe3195f221fd8ef50a3b45d0360afc950ac331
IlzeRieba <78449581+IlzeRieba@users.noreply.github.com>
2022-06-30 13:57:28 +0200

    fixed warnings

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ function App() {
       <Weather defaultCity="New York" />
       <footer>
         This project was coded by Ilze Rieba and is{" "}
-        <a href="https://github.com/IlzeRieba/react-weather-app">
+        <a href="https://github.com/IlzeRieba/react-weather-app" target="_blank" rel="noopener noreferral">
           {" "}
           open-sourced on GitHub{" "}
         </a>
         and hosted on{" "}
-        <a href="https://jade-pegasus-9e9ab7.netlify.app/"> Netlify </a>
+        <a href="https://jade-pegasus-9e9ab7.netlify.app/" target="_blank" rel="noopener noreferral"> Netlify </a>
       </footer>
     </div>
   );
